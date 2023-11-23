72961bf26b548297217ec6661e3df4060b08b68e
Ram-Ram007 <ramajayam2411@gmail.com>
2023-11-23 23:17:39 +0530

    modified

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -12,45 +12,25 @@ const updateStatusController = async function (req, res) {
     rows: pgRes.rows,
     count: pgRes.rowCount,
   });
-};
-
-const getFavController = async (req, res) => {
+};const getFavController = async (req, res) => {
   try {
-    let query;
-
-    //http://localhost:5001/fav?user_id=2
+    let query =
+      "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
 
     if (req.query.user_id) {
-      query =
-        "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
       query += ` WHERE favourites.user_id = ${req.query.user_id}`;
     }
 
-    //http://localhost:5000/fav?user_id=1&search=al
-
     if (req.query.search) {
-      query =
-        "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
       query += ` AND items.item_name ILIKE '%${req.query.search}%'`;
     }
 
-    // Adding sorting
-
     if (req.query.sortOrder) {
-      query =
-        "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
       const sortOrder = req.query.sortOrder === "desc" ? "DESC" : "ASC";
       query += ` ORDER BY ${sortOrder}`;
     }
 
-    // Adding sorting by price range
-    //http://localhost:5001/fav?priceRange=0-500
-
-    if (req.query.user_id.priceRange) {
-      query =
-        "SELECT items.* FROM items JOIN favourites ON items.item_id = favourites.item_id";
-        
-      const userId = req.query.user_id;
+    if (req.query.priceRange) {
       const priceRanges = req.query.priceRange.split("-");
       const minPrice = parseFloat(priceRanges[0]);
       const maxPrice = parseFloat(priceRanges[1]);
@@ -69,6 +49,7 @@ const getFavController = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
 
 const overallRatingController = async (req, res) => {
   const pgRes = await pgClient.query(
